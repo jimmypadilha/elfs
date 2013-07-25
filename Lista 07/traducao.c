151d651b8c80f25e0b197b8c56cf6071e852bb91
weverton <wevertonperdigao@gmail.com>
2013-07-25 15:10:01 -0400

    resolvendo if

--- Lista 07/traducao.c
+++ Lista 07/traducao.c
@@ -1,23 +1,34 @@
 
 #include <stdio.h>
-// Função : Compara 2 numeros e mostra o maior valor
+// Função : Recebe duas strings informa o conteudo, comp!
 // Autor : grupo 4
 // Data : 7/5/2013
 // Seção de Declarações
 // Declaração de variáveis
-n2;floatn1 ;
-// Imprime na tela o texto
+string2;char tam2;int tam1 ;
 void main() {
-printf("Entre com o primeiro numero: ");
-// Recebe o primeiro numero
-scanf(n1);
-// Imprime na tela o texto
-printf("Entre com o segundo numero: ");
-// Recebe o segundo numero
-scanf(n2);
-// Verifica se n1 é maior que n2
-ifn1n2{printf(n2" E maior que: ",,);
-printf(n1" E maior que: ",,);
-else{
-printf(n1" E maior que: ",,);
+printf("Digite uma string: ");
+scanf(string1);
+printf("Digite outra string: ");
+scanf(string2);
+// Imprime as strings
+printf(string1"String 1: ",);
+printf(string2"String 2: ",);
+strlen(string1)
+strlen(string2)// Impressão do número de caracteres de c!
+
+printf(" caracteres.",":",,"O tamanho de ",);
+printf(" caracteres.",":",,"O tamanho de ",);
+// verifica se as strings são do mesmo tamanho
+if(tam1!=tam2){
+printf("As duas strings são de tamanhos diferentes.");
+}else{
+printf("As duas strings são do mesmo tamanho.");
+}// verifica se as strings possuem o mesmo conteudo
+
+if(string1!=string2){
+printf("As duas strings possuem conteúdo diferente.");
+}else{
+printf("As duas strings possuem o mesmo conteúdo.");
+}
 }
